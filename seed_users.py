0ec6d6a56a33813d6da00802d31b975dfbf20485
Rose Mary <rosemarythottuvelil10@gmail.com>
2026-05-28 14:44:54 +0530

    Fixed Render user dashboard issue

--- seed_users.py
+++ seed_users.py
@@ -4,17 +4,19 @@ from werkzeug.security import generate_password_hash
 conn = sqlite3.connect("firewall.db")
 cursor = conn.cursor()
 
+# Analyst User
 cursor.execute("""
-INSERT INTO users (username, password, role)
+INSERT OR IGNORE INTO users (username, password, role)
 VALUES (?, ?, ?)
 """, ("analyst1", generate_password_hash("Analyst@123"), "analyst"))
 
+# Normal User
 cursor.execute("""
-INSERT INTO users (username, password, role)
+INSERT OR IGNORE INTO users (username, password, role)
 VALUES (?, ?, ?)
 """, ("user1", generate_password_hash("User@123"), "user"))
 
 conn.commit()
 conn.close()
 
-print("Users created")
+print("Users created successfully")
